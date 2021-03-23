5eace285eaf23e3b9cacbb09d57b542eb97a79ce
TaekminKim <tackmin080@gmail.com>
2021-03-23 15:44:31 +0900

    set recyclerView into MusicAdapter

--- app/src/main/java/com/example/musicplayer_example/MainActivity.java
+++ app/src/main/java/com/example/musicplayer_example/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.musicplayer_example;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.database.Cursor;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.FrameLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +27,8 @@ public class MainActivity extends AppCompatActivity {
     private FrameLayout frameLayout;
     private RecyclerView recyclerView;
     private RecyclerView recyclerLike;
+    private ArrayList<MusicData> musicDataArrayList;
+    private MusicAdapter musicAdapter;
 
     //현재 액티비티에 있는 프레임레이아웃에 프래그먼트 지정
     @Override
@@ -27,7 +40,68 @@ public class MainActivity extends AppCompatActivity {
         //find id activity_main
         findViewByIdFunc();
         rePlaceFrag();
+
+        //외부접근권한 설정
+        requestPermissionsFunc();
+        //어댑터 설정
+        MusicAdapter musicAdapter = new MusicAdapter(getApplicationContext());
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+
+        recyclerView.setAdapter(musicAdapter);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        musicDataArrayList = findMusic();
+        musicAdapter.setMusicList(musicDataArrayList);
+        musicAdapter.notifyDataSetChanged();
     }
+
+    private void requestPermissionsFunc()
+    {
+        ActivityCompat.requestPermissions(this,
+                new String[]{
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                MODE_PRIVATE);
+    }
+
+    // sdCard 안의 음악을 검색한다
+    public ArrayList<MusicData> findMusic() {
+        ArrayList<MusicData> sdCardList = new ArrayList<>();
+
+        String[] data = {
+                MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.TITLE,
+                MediaStore.Audio.Media.ALBUM_ID,
+                MediaStore.Audio.Media.DURATION};
+
+        // 특정 폴더에서 음악 가져오기
+//        String selection = MediaStore.Audio.Media.DATA + " like ? ";
+//        String selectionArqs = new String[]{"%MusicList%"}
+
+        // 전체 영역에서 음악 가져오기
+        Cursor cursor = getApplicationContext().getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                data, null, null, data[2] + " ASC");
+
+        if (cursor != null) {
+            while (cursor.moveToNext()) {
+
+                // 음악 데이터 가져오기
+                String id = cursor.getString(cursor.getColumnIndex(data[0]));
+                String artist = cursor.getString(cursor.getColumnIndex(data[1]));
+                String title = cursor.getString(cursor.getColumnIndex(data[2]));
+                String albumArt = cursor.getString(cursor.getColumnIndex(data[3]));
+                String duration = cursor.getString(cursor.getColumnIndex(data[4]));
+
+                MusicData mData = new MusicData(id, artist, title, albumArt, duration, 0, 0);
+
+                sdCardList.add(mData);
+            }
+        }
+        return sdCardList;
+    }
+
 
     //현재 액티비티에 있는 프레임 레이아웃에 프래그먼트를 지정한다.
     private void rePlaceFrag()
@@ -39,6 +113,7 @@ public class MainActivity extends AppCompatActivity {
         ft.commit();
     }
 
+
     private void findViewByIdFunc()
     {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
@@ -46,9 +121,4 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerLike = (RecyclerView) findViewById(R.id.recyclerLike);
     }
-
-
-
-
-
 }
